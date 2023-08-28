277455511d6792cf03fddb189fa1e8102cc8a3da
Jim Fowler <fowler@math.osu.edu>
2023-08-27 22:01:38 -0400

    initial import of homework

---
+++ Homework02.lean
@@ -0,0 +1,36 @@
+import Mathlib.Tactic
+
+variable (x y z w : ℕ)
+
+def s02p01 : (x + y) + z = x + (y + z) := by 
+  sorry
+
+def s02p02 : x * y * z * w = w * y * x * z := by 
+  sorry
+
+def s02p03 (h : y = 2 * x) : x + y = 3 * x := by
+  sorry
+
+def s02p04 : x + 0 = x := by 
+  sorry
+
+inductive mynat where
+  | zero : mynat
+  | succ (n : mynat) : mynat
+deriving Repr
+
+open mynat
+
+def one := succ zero
+def two := succ (succ zero)
+def three := succ (succ (succ zero))
+
+def add : mynat → mynat → mynat := fun
+  | m, zero        => m
+  | m, succ n      => succ (add m n)
+  
+def mul : mynat → mynat → mynat := by
+  sorry
+
+def s02p05 : mul two three = add three three := by 
+  sorry
---
+++ lakefile.lean
@@ -0,0 +1,18 @@
+import Lake
+open Lake DSL
+
+package «homework02» {
+  -- add any package configuration options here
+}
+
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git"
+
+@[default_target]
+lean_lib «Homework02» {
+  -- add any library configuration options here
+}
+
+lean_lib «TestRunner» {
+  -- add any library configuration options here
+}
